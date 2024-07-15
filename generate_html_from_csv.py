d50069b3e1a7ef8a09be64cce41ddf82b9cc12f3
xiaoman-zhang <USTCzxm@gmail.com>
2024-07-14 22:10:44 -0400

    update results

--- generate_html_from_csv.py
+++ generate_html_from_csv.py
@@ -9,25 +9,26 @@ def generate_html_from_csv(csv_path, output_path):
     # HTML开头部分
     html_string = '''
     <div class="col-md-12">
-      <div class="infoCard">
-        <div class="infoBody">
-          <div class="infoHeadline">
-            <h2>Leaderboard on MIMIC-CXR Dataset</h2>
-          </div>
-          <p>Info about the dataset.</p>
-          <table class="table performanceTable tablesorter" id="modelTable">
-            <thead>
-              <tr>
-                <th>Rank</th>
-                <th>Model</th>
-                <th>BLEU</th>
-                <th>Bert-Sim</th>
-                <th>RadGraph</th>
-                <th>RadCliQ-v1</th>
-                <th>KG-Sim</th>
-              </tr>
-            </thead>
-            <tbody>
+          <div class="infoCard">
+            <div class="infoBody">
+              <div class="infoHeadline">
+                <h2>Leaderboard on MIMIC-CXR Dataset</h2>
+              </div>
+              <p>MIMIC-CXR contains 377,110 images corresponding to 227,835 radiographic studies performed at the Beth Israel Deaconess Medical Center in Boston, MA.  We follow the official split of MIMIC-CXR in the following experiments.</p>
+              <table class="table performanceTable tablesorter" id="modelTable">
+                <thead>
+                  <tr>
+                    <th>Rank</th>
+                    <th>Model</th>
+                    <th>BLEU</th>
+                    <th>BertScore</th>
+                    <th>SembScore</th>
+                    <th>RadGraph</th>
+                    <th>RadCliQ-v0</th>
+                    <th>RadCliQ-v1</th>
+                  </tr>
+                </thead>
+                <tbody>
     '''
     
     # 生成表格内容
@@ -43,10 +44,11 @@ def generate_html_from_csv(csv_path, output_path):
                   <p class="institution">{row['Institution']}</p>
                 </td>
                 <td><b>{row['BLEU']}</b></td>
-                <td><b>{row['Bert-Sim']}</b></td>
+                <td><b>{row['BertScore']}</b></td>
+                <td><b>{row['SembScore']}</b></td>
                 <td><b>{row['RadGraph']}</b></td>
+                <td><b>{row['RadCliQ-v0']}</b></td>
                 <td><b>{row['RadCliQ-v1']}</b></td>
-                <td><b>{row['KG-Sim']}</b></td>
               </tr>
         '''
     
@@ -66,34 +68,34 @@ def generate_html_from_csv(csv_path, output_path):
 
 import pandas as pd
 
-# Data to create DataFrame
-data = {
-    "Rank": [1, 2, 3, 4, 5, 6, 7, 8],
-    "Date": ["2024-07-13"] * 8,
-    "Model Name": ["CheXpertPlus-Baseline", "MedVersa", "LLM-CXR", "CheXagent", "RadFM", "VLCI", "CvT2DistilGPT2", "RGRG"],
-    "Institution": ["Institution1", "Institution2", "Institution3", "Institution4", "Institution5", "Institution6", "Institution7", "Institution8"],
-    "Model URL": [
-        "https://example.com/model1", 
-        "https://example.com/model2", 
-        "https://example.com/model3", 
-        "https://example.com/model4", 
-        "https://example.com/model5", 
-        "https://example.com/model6", 
-        "https://example.com/model7", 
-        "https://example.com/model8"
-    ],
-    "BLEU": [90.939, 91.939, 89.939, 88.939, 87.939, 86.939, 85.939, 84.939],
-    "Bert-Sim": [93.214, 94.214, 92.214, 91.214, 90.214, 89.214, 88.214, 87.214],
-    "RadGraph": [93.214, 94.214, 92.214, 91.214, 90.214, 89.214, 88.214, 87.214],
-    "RadCliQ-v1": [93.214, 94.214, 92.214, 91.214, 90.214, 89.214, 88.214, 87.214],
-    "KG-Sim": [93.214, 94.214, 92.214, 91.214, 90.214, 89.214, 88.214, 87.214]
-}
+# # Data to create DataFrame
+# data = {
+#     "Rank": [1, 2, 3, 4, 5, 6, 7, 8],
+#     "Date": ["2024-07-13"] * 8,
+#     "Model Name": ["CheXpertPlus-Baseline", "MedVersa", "LLM-CXR", "CheXagent", "RadFM", "VLCI", "CvT2DistilGPT2", "RGRG"],
+#     "Institution": ["Institution1", "Institution2", "Institution3", "Institution4", "Institution5", "Institution6", "Institution7", "Institution8"],
+#     "Model URL": [
+#         "https://example.com/model1", 
+#         "https://example.com/model2", 
+#         "https://example.com/model3", 
+#         "https://example.com/model4", 
+#         "https://example.com/model5", 
+#         "https://example.com/model6", 
+#         "https://example.com/model7", 
+#         "https://example.com/model8"
+#     ],
+#     "BLEU": [90.939, 91.939, 89.939, 88.939, 87.939, 86.939, 85.939, 84.939],
+#     "Bert-Sim": [93.214, 94.214, 92.214, 91.214, 90.214, 89.214, 88.214, 87.214],
+#     "RadGraph": [93.214, 94.214, 92.214, 91.214, 90.214, 89.214, 88.214, 87.214],
+#     "RadCliQ-v1": [93.214, 94.214, 92.214, 91.214, 90.214, 89.214, 88.214, 87.214],
+#     "KG-Sim": [93.214, 94.214, 92.214, 91.214, 90.214, 89.214, 88.214, 87.214]
+# }
 
-# Create DataFrame
-df = pd.DataFrame(data)
+# # Create DataFrame
+# df = pd.DataFrame(data)
 
-# Save DataFrame to CSV
-df.to_csv('./results/result_mimiccxr.csv', index=False)
+# # Save DataFrame to CSV
+# df.to_csv('./results/result_mimiccxr.csv', index=False)
 
 # 调用函数生成HTML文件
 generate_html_from_csv('./results/result_mimiccxr.csv', './results/table_mimiccxr.html')
